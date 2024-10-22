931159387a096468010f385c5b181b99c602e095
alunojava-26 <solcamarques@outlook.com>
2024-10-22 17:29:59 -0300

    adicionando mundo dois

---
+++ java/src/main/java/com/geracaocaldeira/java/MundodoisApplication.java
@@ -0,0 +1,13 @@
+package com.geracaocaldeira.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundodoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundodoisApplication.class, args);
+	}
+
+}
